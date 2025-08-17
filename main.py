2b013b53f628fadd8d6ae725198aee2bf3d54b8b
idk <kanexionn@gmail.com>
2025-08-17 21:21:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -202,4 +202,5 @@ if __name__ == "__main__":
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("ping", ping))
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
-    app.run_polling()
+    #app.run_polling()
+
